4675772760575abecd4ec585ab04ef17973cfa4f
Jaakko Pallari <jkpl@lepovirta.org>
2012-08-14 19:46:50 +0300

    Refactoring...

--- yahe.js
+++ yahe.js
@@ -8,6 +8,123 @@
     hintcharacters: 'fdjkghslrueicnxmowabzpt'
   }, doc = window.document;
 
+  // YAHE constructor
+  var mk_yahe = function() {
+    var that = {}, hintsobj, input = "",
+        active = false, selected_el;
+
+    that.newhints = function() {
+      that.deactivate();
+      hintsobj = create_hints();
+      doc.body.appendChild(hintsobj.container);
+      active = true;
+    };
+
+    that.deactivate = function() {
+      if (active) {
+        doc.body.removeChild(hintsobj.container);
+        that.clear_input();
+        active = false;
+      }
+    };
+
+    that.clear_input = function() {
+      input = "";
+      dehilight();
+      selected_el = undefined;
+    };
+
+    that.append_input = function(c) {
+      input += c;
+      select_hint(input);
+    };
+
+    that.keydown = function(e) {
+      var handler = keyhandlers[e.keyCode], r;
+      if (!handler)
+        handler = keyhandlers['default'];
+      r = handler(e);
+      if (r) {
+        e.stopPropagation();
+        e.preventDefault();
+      }
+    };
+
+    var open_selected = function(e) {
+      if (selected_el) {
+        var node = selected_el.node;
+        if (node.tagName === 'INPUT' || node.tagName === 'TEXTAREA') {
+          node.focus();
+          that.deactivate();
+        } else {
+          mouseclick(node, e.ctrlKey, e.altKey, e.shiftKey, e.metaKey);
+        }
+      }
+    };
+
+    var select_hint = function(query) {
+      var el = hintsobj.hints[query.toLowerCase()];
+      if (typeof el !== 'undefined') {
+        dehilight();
+        selected_el = el;
+        hilight();
+      }
+    };
+
+    var dehilight = function() {
+      if (selected_el) {
+        var re = new RegExp("(\\s|^)" + globals.hilight_class + "(\\s|$)");
+        selected_el.span.className = selected_el.span.className.replace(re, '');
+      }
+    };
+
+    var hilight = function() {
+      if (selected_el)
+        selected_el.span.className += ' ' + globals.hilight_class;
+    };
+
+    var keyhandlers = {
+      default: function(e) {
+        if (active && !has_modifiers(e)) {
+          var c = String.fromCharCode(e.keyCode).toLowerCase();
+          if (globals.hintcharacters.indexOf(c) >= 0)
+            that.append_input(c);
+          e.preventDefault();
+          e.stopPropagation();
+          return true;
+        }
+        return false;
+      },
+      27: function(e) { // escape
+        that.deactivate();
+        return true;
+      },
+      13: function(e) { // return
+        open_selected(e);
+        that.clear_input();
+        return true;
+      }
+    };
+    keyhandlers[globals.activate_keycode] = function(e) {
+      var r = true;
+      if (active) {
+        if (e.ctrlKey && input.length > 0)
+          that.clear_input();
+        else if (e.ctrlKey)
+          that.deactivate();
+        else
+          r = keyhandlers.default(e);
+      } else if (e.ctrlKey) {
+        that.newhints();
+      }
+      return r;
+    };
+
+    return that;
+  };
+
+  // Helper functions
+
   var forEach = function(list, cb) {
     for (var i = 0; i < list.length; i++)
       cb(list[i], i, list);
@@ -76,116 +193,11 @@
     return gen_hintid;
   };
 
-  var dehilight = function(el) {
-    if (el instanceof HTMLElement) {
-      var re = new RegExp("(\\s|^)" + globals.hilight_class + "(\\s|$)");
-      el.className = el.className.replace(re, '');
-    }
+  var has_modifiers = function(e) {
+    return (e.ctrlKey || e.altKey || e.metaKey);
   };
 
-  var hilight = function(el) {
-    if (el instanceof HTMLElement)
-      el.className += ' ' + globals.hilight_class;
-  };
-
-  var is_yahekey = function(e) {
-    return (e.ctrlKey && e.keyCode === globals.activate_keycode);
-  };
-
-  var mk_yahe = function() {
-    var that = {}, hintsobj, input = "",
-        active = false, selected_el;
-
-    that.newhints = function() {
-      that.deactivate();
-      hintsobj = create_hints();
-      doc.body.appendChild(hintsobj.container);
-      active = true;
-    };
-
-    that.deactivate = function() {
-      if (active) {
-        doc.body.removeChild(hintsobj.container);
-        that.clear_input();
-        active = false;
-      }
-    };
-
-    that.clear_input = function() {
-      input = "";
-      if (selected_el) {
-        dehilight(selected_el.span);
-        selected_el = undefined;
-      }
-    };
-
-    that.append_input = function(c) {
-      input += c;
-      select_hint(input);
-    };
-
-    that.keydown = function(e) {
-      if (active) {
-        handle_input(e);
-      } else if (is_yahekey(e)) {
-        that.newhints();
-        e.preventDefault();
-        e.stopPropagation();
-      }
-    };
-
-    var handle_input = function(e) {
-      var c;
-      e.preventDefault();
-      e.stopPropagation();
-      switch (e.keyCode) {
-      case globals.activate_keycode:
-        if (e.ctrlKey && input.length > 0) {
-          that.clear_input();
-        } else if (e.ctrlKey) {
-          that.deactivate();
-        }
-        break;
-      case 27:
-        that.deactivate();
-        break;
-      case 13:
-        open_selected(e);
-        that.clear_input();
-        break;
-      default:
-        c = String.fromCharCode(e.keyCode).toLowerCase();
-        if (globals.hintcharacters.indexOf(c) >= 0)
-          that.append_input(c);
-        break;
-      }
-    };
-
-    var open_selected = function(e) {
-      if (selected_el) {
-        var node = selected_el.node;
-        if (node.tagName === 'INPUT' || node.tagName === 'TEXTAREA') {
-          node.focus();
-          that.deactivate();
-        } else {
-          mouseclick(node, e.ctrlKey, e.altKey, e.shiftKey, e.metaKey);
-        }
-      }
-    };
-
-    var select_hint = function(query) {
-      var el = hintsobj.hints[query.toLowerCase()];
-      if (typeof el !== 'undefined') {
-        if (selected_el)
-          dehilight(selected_el.span);
-        selected_el = el;
-        hilight(el.span);
-      }
-    };
-
-    return that;
-  };
-
+  // create and bind YAHE
   var yahe = mk_yahe();
   doc.addEventListener('keydown', yahe.keydown, true);
 }).call(null, window);
